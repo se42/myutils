1aa54ea7561e2e102077fd4517f4579ea25e6dc0
Scott Edwards <scott.edwards3@gmail.com>
2016-03-29 14:22:29 -0400

    complete git setup in djroku.py

--- djroku.py
+++ djroku.py
@@ -3,9 +3,6 @@ import subprocess as sp
 import sys
 import venv
 
-import sh
-
-
 section = '**********************************************************************'
 
 # Confirm script is being run from the correct location
@@ -50,8 +47,8 @@ os.system('../myutils/djroku.sh {0} {1}'.format(temps[temp]['github_url'], proje
 print(section)
 # Manually edit html templates.  CMD-F to find PROJECT_NAME and replace with the actual project name
 print('We need to manually edit the html templates.')
-print('The templates/ directory should have opened automatically.')
-print('CMD-F each template and replace PROJECT_NAME with the actual project name.')
+print('All html files in the templates/ directory should have opened automatically.')
+print('CMD-F each template and replace PROJECT_NAME with the project name: {0}'.format(project))
 
 ready = False
 while not ready:
@@ -62,7 +59,34 @@ while not ready:
 		print('Try again...')
 
 print(section)
-print('On to git and Heroku tomorrow!')
+print('Setting up git repositories...')
+os.system('git init')
+os.system('git add -A')
+os.system('git commit -m "initial commit"')
+os.system('../myutils/gitcreate.sh')
+os.system('git push -u origin master')
+os.system('git checkout -b dev-master')
+os.system('git push -u origin dev-master')
+print('Git repository setup complete.  Local machine is on branch dev-master')
+print(section)
+print('Now for Heroku setup!')
+# heroku create PROJECT_NAME
+# heroku create dev-PROJECT_NAME
+# pause to do the following
+# open app and set SECRET_KEY config variables in heroku app
+# point heroku apps at corresponding git branch and set auto deploys
+# hit the button to deploy both apps to Heroku from Github
+# wait for both to finish
+# press key to continue when done
+# heroku run --app PROJECT_NAME python manage.py migrate
+# heroku run --app PROJECT_NAME python manage.py createsuperuser
+# heroku run --app dev-PROJECT_NAME python manage.py migrate
+# heroku run --app dev-PROJECT_NAME python manage.py createsuperuser
+# function to copy local_settings_template.py into PROJECT_NAME/PROJECT_NAME/local_settings.py
+#	but require user to enter a SECRET_KEY (not the production one!) and the DB_URL so
+#	they can be automatically updated
+# cp projects/myutils/local_settings_template.py projects/PROJECT_NAME/PROJECT_NAME/local_settings.py
+# echo "You're ready to go!"
 
 
 
